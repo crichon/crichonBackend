f4738d15503c54a1183bb01f080190adc1c70d5f
crichon <github@crichon.eu>
2013-12-30 14:37:35 +0100

    polymorphic style

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship, backref
+from sqlalchemy import create_engine, ForeignKey, Column, Integer, String, Table
 from app import app
 
 
@@ -16,4 +16,32 @@ Model.query = db_session.query_property()
 
 def init_db():
     Model.metadata.create_all(bind=engine)
+
+
+user_content = Table('user_content', Model.metadata,
+            Column('content_id', Integer, ForeignKey('contents.id')),
+            Column('user_id', Integer, ForeignKey('users.id'))
+)
+
+
+class Content(Model):
+    __tablename__ = 'contents'
+    id = Column(Integer, primary_key=True)
+    discriminator = Column('type', String(50))
+    users = relationship("User", secondary= user_content, backref="contents")
+    __mapper_args__ = {'polymorphic_on': discriminator}
+
+try: 
+	import app.database.tag.model.tag
+	tag_available = False
+except ImportError:
+	# log error, dev a logger
+	tag_available = False
+
+
+if tag_available:
+    tag_item = Table('tag_todo', Model.metadata,
+            Column('tag_id', Integer, ForeignKey('tags.id')),
+            Column('todo_id', Integer, ForeignKey('users.id'))
+    )
 
--- app/todo/model.py
+++ app/todo/model.py
@@ -1,61 +1,31 @@
-from sqlalchemy import Boolean, ForeignKey, Column, Integer, DateTime, String, Unicode, Table
+from sqlalchemy import Boolean, ForeignKey, Column, Integer, String
 from sqlalchemy.orm import relationship, backref
-from app.database import Model
-
-# find a cleaner way (how to handle database migration ??)
-try: 
-	import app.database.tag.model.tag
-	tag_available = False
-except ImportError:
-	# log error, dev a logger
-	tag_available = False
+from app.database import Content, user_content 
 
 
-user_item = Table('user_todo', Model.metadata,
-            Column('todo_id', Integer, ForeignKey('todo_items.id')),
-            Column('user_id', Integer, ForeignKey('users.id'))
-)
-
-if tag_available:
-    tag_item = Table('tag_todo', Model.metadata,
-            Column('tag_id', Integer, ForeignKey('tags.id')),
-            Column('todo_id', Integer, ForeignKey('users.id'))
-    )
-
-# Organize data by tag or tag and list ?
-#class list(Model):
-    #__tablename__ = "todo_list"
-    #id = Column(Integer, primary_key = True)
-    #title = Column(String(32), index = True)
-    #items = relationship("Item", backref="parent")
-
-
-class Todo(Model):
-    __tablename__ = "todo_items"
-    id = Column(Integer, primary_key = True)
+class Todo(Content):
+    __tablename__ = "todos"
+    __mapper_args__ = {'polymorphic_identity': 'todo'}
+    id = Column(Integer, ForeignKey('contents.id'), primary_key=True)
     title = Column(String(32), unique = True)
-    content = Column(String)
+    text = Column(String)
     done = Column(Boolean)
+    todo_users = relationship("User", secondary= user_content, backref="todos")
 
     @property
     def serealize(self):
 		return {
 				'id': self.id,
 				'title': self.title,
-				'content': self.content,
+				'content': self.text,
 				'done': self.done
 		}
 
     def __init__(self, title, content):
 		self.title = title
-		self.content = content
+		self.text = content
 		self.done = False
 
     def __repr__(self):
-        return "<Todo('%i', '%s', '%s')>" % (self.id, self.title, self.content)
-
-    users = relationship("User", secondary= user_item, backref="todos")
-	# add ref to tag
-    if tag_available:
-		tags = relationship("Tag", secondary=tag_item, backref="todos")
+        return "<Todo('%i', '%s', '%s')>" % (self.id, self.title, self.text)
 
